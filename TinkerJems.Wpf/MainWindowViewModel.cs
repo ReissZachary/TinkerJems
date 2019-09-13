b9d3a0cf4d890693cfba6c25bf31b2471256c404
Zachary Reiss <zachary.reiss123@gmail.com>
2019-09-13 01:12:16 -0600

    Added more buttons to WPF app and made minor changes to web app

--- TinkerJems.Wpf/MainWindowViewModel.cs
+++ TinkerJems.Wpf/MainWindowViewModel.cs
@@ -1,16 +1,38 @@
-﻿using Prism.Mvvm;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Prism.Commands;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using TinkerJems.Core.Models;
 
 namespace TinkerJems.Wpf
 {
     public class MainWindowViewModel : BindableBase
     {
-        
+        public MainWindowViewModel()
+        {           
+
+            ViewRings = new DelegateCommand(
+                () =>
+                {
+                    //execute
+                    //string testMessage = "ADDED TODO";
+                    //MessageBox.Show(testMessage);
+                },
+                () =>
+                {
+                    //can execute
+                    return false;
+                }
+                );
+        }
+        public DelegateCommand ViewRings { get; }
+
+
     }
 }
