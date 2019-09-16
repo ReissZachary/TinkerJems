67da7004a2886f53c4566b2ff0abecd443dcc66e
Zachary Reiss <zachary.reiss123@gmail.com>
2019-09-15 23:25:02 -0600

    Added Edit/Add UserControls and linked them to buttons on main page

--- TinkerJems.Wpf/MainWindow.xaml.cs
+++ TinkerJems.Wpf/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace TinkerJems.Wpf
 {   
@@ -12,6 +13,99 @@ namespace TinkerJems.Wpf
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void AddNewJewelryItem_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            string command = string.Empty;
+
+            //checks to see command OR checks to see if user control
+            if(btn.Tag != null)
+            {
+                command = btn.Tag.ToString();
+                if (command.Contains("."))
+                {
+                    LoadUserControl(command);
+                }
+                else
+                {
+                    ProcessButtonsCommand(command);
+                }
+            }
+        }
+
+        private void EditJewelryItem_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            string command = string.Empty;
+
+            //checks to see command OR checks to see if user control
+            if (btn.Tag != null)
+            {
+                command = btn.Tag.ToString();
+                if (command.Contains("."))
+                {
+                    LoadUserControl(command);
+                }
+                else
+                {
+                    ProcessButtonsCommand(command);
+                }
+            }
+        }
+
+        public void CloseUserControl()
+        {
+            contentArea.Children.Clear();
+        }
+
+        public void DisplayUserControl(UserControl userControl)
+        {
+            //close current control
+            CloseUserControl();
+
+            //adds new control to screen
+            contentArea.Children.Add(userControl);
+
+        }
+
+        public void LoadUserControl(string controlName)
+        {
+            Type  usercontrolType = null;
+            UserControl userControl = null;
+
+            //create type from control type parameter
+            usercontrolType = Type.GetType(controlName);
+            if (usercontrolType == null)
+            {
+                MessageBox.Show(controlName + "doesn't exist!");
+            }
+            else
+            {
+                //create new instance of control
+                userControl = (UserControl)Activator.CreateInstance(usercontrolType);
+                if (userControl != null)
+                {
+                    //display wanted user control
+                    DisplayUserControl(userControl);
+                }
+            }
+        }
+
+        public void ProcessButtonsCommand(string command)
+        {
+            switch (command.ToLower())
+            {
+                case "exit":
+                    this.Close();
+                    break;
+
+                default:
+                    break;
+
+            }
         }
     }
 }
--- TinkerJems.Wpf/MainWindowViewModel.cs
+++ TinkerJems.Wpf/MainWindowViewModel.cs
@@ -4,6 +4,8 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +17,7 @@ namespace TinkerJems.Wpf
     public class MainWindowViewModel : BindableBase
     {
         public MainWindowViewModel()
-        {           
+        {
 
             ViewRings = new DelegateCommand(
                 () =>
