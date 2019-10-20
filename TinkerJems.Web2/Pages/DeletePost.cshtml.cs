42b4aab1daf4b9c6f467fb98dfe44c1fd3d245b4
Zachary Reiss <zachary.reiss123@gmail.com>
2019-10-19 22:55:09 -0600

    Delete Post is working. All blog functionality is working

---
+++ TinkerJems.Web2/Pages/DeletePost.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TinkerJems.Core.Models;
+using TinkerJems.Web2.Data;
+
+namespace TinkerJems.Web2.Pages
+{
+    public class DeletePostModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DeletePostModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public TinkerJemsBlogPost BlogPost { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            BlogPost = await _context.TinkerJemsBlogPost.FindAsync(id);
+
+            if (BlogPost == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            BlogPost = await _context.TinkerJemsBlogPost.FindAsync(id);
+
+            if (BlogPost != null)
+            {
+                _context.TinkerJemsBlogPost.Remove(BlogPost);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Blogs");
+        }
+
+    }
+}
