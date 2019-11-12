dec9a3cda1f5942c910bee9126413effc2452c2b
Zachary Reiss <zachary.reiss123@gmail.com>
2019-11-11 19:55:11 -0700

    HomeViewModel

---
+++ TinkerJems.Wpf.Application/ViewModels/HomeViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TinkerJems.Wpf.Application.ViewModels
+{
+    public class HomeViewModel : BindableBase
+    {
+        private readonly IRegionManager _regionManager;
+        public DelegateCommand<string> NavigateToSearch { get; }
+
+        public HomeViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+            NavigateToSearch = new DelegateCommand<string>((uri) =>
+            {
+                _regionManager.RequestNavigate("NavigationRegion", uri);
+            });
+        }
+    }
+}
