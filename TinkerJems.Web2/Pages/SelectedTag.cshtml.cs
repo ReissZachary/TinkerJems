3d2861185bd1dc11e90c01341707685dbd3b85d6
Zachary Reiss <zachary.reiss123@gmail.com>
2019-11-18 10:46:57 -0700

    Created SelectedTag page

--- TinkerJems.Web2/Pages/SelectedTag.cshtml.cs
+++ TinkerJems.Web2/Pages/SelectedTag.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using TinkerJems.Core.Models;
 using TinkerJems.Web2.Data;
 
@@ -13,16 +14,20 @@ namespace TinkerJems.Web2.Pages
     {
         private readonly ApplicationDbContext _context;
 
-        public List<JewelryItem> JewelryItems { get; }
+        public List<JewelryItem> JewelryItems { get; set; }
+        public Tag Tag { get; private set; }
 
         public SelectedTagModel(ApplicationDbContext context)
         {
             _context = context;
             JewelryItems = new List<JewelryItem>();
         }
-        public void OnGet()
+        public async Task OnGetAsync(string tagName)
         {
-
+            Tag = await _context.Tags.Include(t => t.TaggedItems)
+                .ThenInclude(i => i.JewelryItem)
+                .FirstOrDefaultAsync(t => t.Name == tagName);
+            JewelryItems.AddRange(Tag.TaggedItems.Select(t => t.JewelryItem));
         }
     }
 }
