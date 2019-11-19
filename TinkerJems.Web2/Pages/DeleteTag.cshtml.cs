d7ae7c9dcb0d806a7f52dde92c06f3447396e320
Zachary Reiss <zachary.reiss123@gmail.com>
2019-11-18 23:52:44 -0700

    Attempt to get deletion of tag working

---
+++ TinkerJems.Web2/Pages/DeleteTag.cshtml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TinkerJems.Core.Models;
+using TinkerJems.Web2.Data;
+
+namespace TinkerJems.Web2.Pages
+{
+    public class DeleteTagModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        public DeleteTagModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<JewelryItemTag> ItemTags { get; private set; }
+        public IEnumerable<JewelryItemTag> SelectedTag { get; set; }
+
+        public JewelryItem JewelryItem { get; set; }
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            JewelryItem = await _context.JewelryItems
+                            .Include(j => j.Tags)
+                            .ThenInclude(t => t.Tag)
+                            .FirstOrDefaultAsync(m => m.Id == id);
+            ItemTags = JewelryItem.Tags;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string tagName)
+        {
+            SelectedTag = ItemTags.Where(t => t.Tag.Name == tagName);
+
+            if (tagName == null)
+            {
+                return NotFound();
+            }
+            if (JewelryItem != null)
+            {
+                //_context.JewelryItems.Remove(JewelryItem);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToPage("./Items");
+        }
+
+    }
+}
