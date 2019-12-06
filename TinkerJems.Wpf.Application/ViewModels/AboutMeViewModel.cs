273c85f666f5bca5c787e80b8f383ac93ae07d6e
Zachary Reiss <zachary.reiss123@gmail.com>
2019-12-05 18:15:28 -0700

    add hyperlinks to about me - not working

--- TinkerJems.Wpf.Application/ViewModels/AboutMeViewModel.cs
+++ TinkerJems.Wpf.Application/ViewModels/AboutMeViewModel.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Diagnostics;
+using System.Windows.Navigation;
+using Prism.Mvvm;
+using Prism.Commands;
 
 namespace TinkerJems.Wpf.Application.ViewModels
 {
-    public class AboutMeViewModel
+    public class AboutMeViewModel : BindableBase
     {
+        public AboutMeViewModel()
+        {
 
+        }
+
+        private DelegateCommand personalInstagram;
+        public DelegateCommand PersonalInstagram => personalInstagram ?? (personalInstagram = new DelegateCommand(
+                () =>
+                {
+                    Process.Start("Chrome.exe", "https://www.instagram.com/nettie.graham/");
+                }
+            ));
     }
 }
